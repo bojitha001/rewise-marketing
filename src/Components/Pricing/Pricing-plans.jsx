80bc9dd4cbacf4f005a5437e3f73d935720fddee
Lashan Gunawardena <ravijalashangunawardena@gmail.com>
2026-03-23 02:12:16 +0530

    added a tag in the pricing plans

--- src/Components/Pricing/Pricing-plans.jsx
+++ src/Components/Pricing/Pricing-plans.jsx
@@ -32,7 +32,7 @@ const PricingPlans = () => {
                 onClick={() => setIsAnnual(false)}
                 className={`cursor-pointer transition ${
                 !isAnnual
-                    ? "font-semibold text-[#0040c1]"
+                    ? "font-semibold text-[#0040c1] underline underline-offset-15"
                     : "text-gray-400 hover:text-black"
                 }`}
             >
@@ -43,7 +43,7 @@ const PricingPlans = () => {
                 onClick={() => setIsAnnual(true)}
                 className={`cursor-pointer transition ${
                 isAnnual
-                    ? "font-semibold text-[#0040c1]"
+                    ? "font-semibold text-[#0040c1] underline underline-offset-15"
                     : "text-gray-400 hover:text-black"
                 }`}
             >
@@ -64,8 +64,13 @@ const PricingPlans = () => {
                   : "border-gray-200"
               }`}
             >
-              <h3 className="text-2xl font-semibold mb-2">
+              <h3 className="flex justify-between items-center text-2xl font-semibold mb-2">
                 {plan.name}
+                {plan.highlighted && (
+                  <div className="text-base font-normal text-white bg-blue-500 rounded-full px-3 py-1 flex items-center gap-1">
+                    <span className="text-white">★</span> Most popular
+                  </div>
+                )}
               </h3>
 
               <p className="text-4xl font-bold mb-4">
